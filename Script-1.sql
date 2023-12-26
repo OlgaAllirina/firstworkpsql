cf5b3fcf3408cc0190e065c96b4861f30feb40da
OlgaAllirina <141494354+OlgaAllirina@users.noreply.github.com>
2023-12-26 11:39:59 +0300

    Update Script-1.sql
    error correction

--- Script-1.sql
+++ Script-1.sql
@@ -8,39 +8,33 @@ performer_id SERIAL primary key,
 name VARCHAR(60) not NULL
 );
 
-CREATE table if not exists Performer_Genre (
-performer INTEGER not NULL REFERENCES Musician_performer(performer_id),
-genre INTEGER not NULL REFERENCES Musical_genre(genre_id),
-constraint performer_genre_pk primary KEY(performer, genre)
-);
-
 CREATE table if not exists Album (
 album_id SERIAL primary key,
 name VARCHAR(80) not NULL,
-date INTEGER not NULL
-);
-
-CREATE table if not exists Album_performer (
-album INTEGER not NULL REFERENCES Album(album_id),
-performer INTEGER not NULL REFERENCES Musician_performer(performer_id),
-constraint album_performer_pk primary KEY(album, performer)
+year_of_issue VARCHAR(4) not null
 );
 
 CREATE table if not exists Track (
 track_id SERIAL primary key ,
-name VARCHAR(80) UNIQUE not NULL,
-duration INTEGER not NULL
+name VARCHAR(40) not NULL,
+duration TIME not null,
+album INTEGER not NULL references Album(album_id) 
+);
+
+CREATE TABLE IF NOT EXISTS Performer_Genre (
+genre INTEGER NOT NULL references Musical_genre(genre_id),
+performer INTEGER NOT NULL references Musician_performer(performer_id)
+);
+
+CREATE table if not exists Album_performer (
+album INTEGER not NULL REFERENCES Album(album_id),
+performer INTEGER not NULL REFERENCES Musician_performer(performer_id)
 );
 
 CREATE table if not exists Collection (
 collection_id SERIAL primary key,
 name VARCHAR(60) not NULL,
-year_of_issue date 
-);
-
-CREATE table if not exists Track_collection (
-track INTEGER not NULL REFERENCES Track(track_id),
-collection INTEGER not NULL REFERENCES Collection(collection_id),
-constraint track_collection_pk primary KEY(track, collection)
+year_of_issue VARCHAR(4) not null,
+track INTEGER NOT NULL references Track(track_id)
 );
 
